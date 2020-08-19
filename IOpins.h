82ed243738f2effea9c974fcaf83ca50bb5b8c99
incubus-ank <kpahyx@gmail.com>
2020-08-20 00:29:22 +0300

    Initial commit

---
+++ IOpins.h
@@ -0,0 +1,21 @@
+
+#define LmotorA             3  // Левый мотор H мост, вход A
+#define LmotorB            11  // Левый мотор H мост, вход B
+#define RmotorA             5  // Правый мотор H мост, вход A
+#define RmotorB             6  // Правый мотор H мост, вход B
+
+#define RCleft              0  // Цифровой вход 0
+#define RCright             1  // Цифровой вход 1
+
+#define S0                  2  // Серво выход 00
+#define S1                  4  // Серво выход 01
+#define S2                  7  // Серво выход 02
+#define S3                  8  // Серво выход 03
+#define S4                  9  // Серво выход 04
+#define S5                 10  // Серво выход 05
+#define S6                 12  // Серво выход 06
+
+#define Battery             0  // Аналоговый вход 00
+#define RmotorC             6  // Аналоговый вход 06
+#define LmotorC             7  // Аналоговый вход 07
+#define Charger            13  // Low= ON High = OFF
